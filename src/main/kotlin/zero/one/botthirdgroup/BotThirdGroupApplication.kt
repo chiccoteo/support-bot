0d59bd2bf2b5d237df7c428f4e99503bd5c8718f
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2023-07-16 09:20:46 +0500

    first commit

---
+++ src/main/kotlin/zero/one/botthirdgroup/BotThirdGroupApplication.kt
@@ -0,0 +1,11 @@
+package zero.one.botthirdgroup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BotThirdGroupApplication
+
+fun main(args: Array<String>) {
+    runApplication<BotThirdGroupApplication>(*args)
+}
---
+++ src/test/kotlin/zero/one/botthirdgroup/BotThirdGroupApplicationTests.kt
@@ -0,0 +1,13 @@
+package zero.one.botthirdgroup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BotThirdGroupApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
