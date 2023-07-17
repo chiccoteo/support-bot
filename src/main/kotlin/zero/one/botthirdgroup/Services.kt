a0ea200606ba5939af46bbad8d00a3259949d781
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2023-07-17 11:43:21 +0500

    initialize user service

--- src/main/kotlin/zero/one/botthirdgroup/Services.kt
+++ src/main/kotlin/zero/one/botthirdgroup/Services.kt
@@ -1,26 +1,44 @@
 package zero.one.botthirdgroup
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
-interface MessageService {
-
-    fun create(messageCreateDTO: MessageCreateDTO): GetToMessageDTO
-
-    fun get(): GetMessageDTO
-
+interface UserService {
+    fun create(dto: UserCreateDto)
+    fun update(id: Long, dto: UserUpdateDto)
+    fun getOne(id: Long): GetOneUserDto
+    fun getAll(pageable: Pageable): Page<GetOneUserDto>
+    fun delete(id: Long)
 }
 
 @Service
-class MessageServiceImpl() : MessageService {
-    override fun create(messageCreateDTO: MessageCreateDTO): GetToMessageDTO {
+class UserServiceImpl(
+    private val userRepository: UserRepository
+) : UserService {
+    override fun create(dto: UserCreateDto) {
+        dto.run {
+            if (userRepository.existsByChatId(chatId)) throw ChatIdExistsException(chatId)
+//            userRepository.save(toEntity())
+        }
+    }
+
+    override fun update(id: Long, dto: UserUpdateDto) {
         TODO("Not yet implemented")
     }
 
-    override fun get(): GetMessageDTO {
+    override fun getOne(id: Long): GetOneUserDto {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAll(pageable: Pageable): Page<GetOneUserDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(id: Long) {
         TODO("Not yet implemented")
     }
 }
-
 
 interface MessageService {
 
